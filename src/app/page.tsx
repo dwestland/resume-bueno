4ee06aeaf40f242d9eca49c4e3acdfaadfe2b7f5
dwestland <don@westland.net>
2025-02-19 22:11:13 -0800

    Update home page with SessionProvider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,16 @@
-'use server'
-import { auth } from '@/auth'
-import Link from 'next/link'
-import { prisma } from '@/lib/prisma'
+'use client'
 
-export default async function Home() {
-  const session = await auth()
+import { useSession } from 'next-auth/react'
+import Link from 'next/link'
+
+export default function ClientHome() {
+  const { data: session, status } = useSession()
+
+  if (status === 'loading') {
+    return <div>Loading...</div>
+  }
 
   if (session?.user && session.user.email) {
-    // Fetch the user's resume field and credits
-    const userRecord = await prisma.user.findUnique({
-      where: { email: session.user.email },
-      select: {
-        resume: true,
-        credits: true,
-      },
-    })
-
-    const isResume = userRecord?.resume
-
     return (
       <div className="p-4">
         <h1 className="text-2xl font-bold mb-2">Resume Bueno</h1>
@@ -25,22 +18,12 @@ export default async function Home() {
           Resume&nbsp; &nbsp;+&nbsp; &nbsp;Job Description&nbsp; &nbsp;=&nbsp;
           &nbsp;Resume Bueno
         </h2>
-        {!isResume && (
-          <Link
-            href="/resume/add"
-            className="inline-block bg-blue-500 hover:bg-blue-600 text-white hover:text-white px-4 py-2 rounded mb-4"
-          >
-            Get Started
-          </Link>
-        )}
-        {isResume && (
-          <Link
-            href="/custom-resume"
-            className="inline-block bg-blue-500 hover:bg-blue-600 text-white hover:text-white px-4 py-2 rounded mb-4"
-          >
-            Make Custom Resume
-          </Link>
-        )}
+        <Link
+          href="/custom-resume"
+          className="inline-block bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded mb-4"
+        >
+          Make Custom Resume
+        </Link>
       </div>
     )
   }
@@ -54,7 +37,7 @@ export default async function Home() {
       </h2>
       <Link
         href="/resume/add"
-        className="inline-block bg-blue-500 hover:bg-blue-600 text-white hover:text-white px-4 py-2 rounded mb-4"
+        className="inline-block bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded mb-4"
       >
         Get Started
       </Link>
