8c585a003142abdf29fd740b45fe9d928e8a9839
dwestland <don@westland.net>
2025-02-21 14:31:39 -0800

    Update UI on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useSession } from 'next-auth/react'
 import Link from 'next/link'
+import { Button } from '@/components/ui/button'
 
 export default function ClientHome() {
   const { data: session, status } = useSession()
@@ -13,17 +14,14 @@ export default function ClientHome() {
   if (session?.user && session.user.email) {
     return (
       <div className="p-4">
-        <h1 className="text-2xl font-bold mb-2">Resume Bueno</h1>
-        <h2 className="text-xl font-bold mb-4">
+        <h1>Resume Bueno</h1>
+        <h2>
           Resume&nbsp; &nbsp;+&nbsp; &nbsp;Job Description&nbsp; &nbsp;=&nbsp;
           &nbsp;Resume Bueno
         </h2>
-        <Link
-          href="/custom-resume"
-          className="inline-block bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded mb-4"
-        >
-          Make Custom Resume
-        </Link>
+        <Button asChild>
+          <Link href="/custom-resume">Make Custom Resume</Link>
+        </Button>
       </div>
     )
   }
@@ -35,12 +33,19 @@ export default function ClientHome() {
         Resume&nbsp; &nbsp;+&nbsp; &nbsp;Job Description&nbsp; &nbsp;=&nbsp;
         &nbsp;Resume Bueno
       </h2>
-      <Link
-        href="/resume/add"
-        className="inline-block bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded mb-4"
-      >
-        Get Started
-      </Link>
+      <h1>Resume Bueno</h1>
+      <h2>
+        Resume&nbsp; &nbsp;+&nbsp; &nbsp;Job Description&nbsp; &nbsp;=&nbsp;
+        &nbsp;Resume Bueno
+      </h2>
+      <Button>
+        <Link
+          href="/resume/add"
+          // className="inline-block bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded mb-4"
+        >
+          Get Started
+        </Link>
+      </Button>
     </div>
   )
 }
