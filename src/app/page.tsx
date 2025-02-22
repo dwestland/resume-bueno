72c64e13d6d1284420081ccd23188e74cbc56518
dwestland <don@westland.net>
2025-02-22 14:14:14 -0800

    Update homepage UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,27 +14,59 @@ export default function ClientHome() {
   if (session?.user && session.user.email) {
     return (
       <div className="p-4">
+        <p
+          className="text-2xl font-semibold tracking-tight text-violet-800"
+          style={{
+            fontFamily: 'var(--font-oswald)',
+            marginBottom: '-7px',
+          }}
+        >
+          BETA
+        </p>
         <h1>Resume Bueno</h1>
         <h2>Resume&nbsp;+&nbsp;Job Description&nbsp;=&nbsp;Resume Bueno</h2>
+
         <Button asChild>
           <Link href="/custom-resume">Make Custom Resume</Link>
         </Button>
+
+        <h2>Land More Interviews, Effortlessly</h2>
+        <p className="text-xl">
+          Upload your resume, add a job description, and let Resume Bueno do the
+          rest. Get a match score, targeted resume improvements, and a polished
+          cover letter, instantly.
+        </p>
       </div>
     )
   }
 
   return (
     <div className="p-4">
+      <p
+        className="text-2xl font-semibold tracking-tight text-violet-800"
+        style={{
+          fontFamily: 'var(--font-oswald)',
+          marginBottom: '-7px',
+        }}
+      >
+        BETA
+      </p>
       <h1>Resume Bueno</h1>
       <h2>Resume&nbsp;+&nbsp;Job Description&nbsp;=&nbsp;Resume Bueno</h2>
       <Button>
         <Link
           href="/resume/add"
-          // className="inline-block bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded mb-4"
+          // className="inline-block px-4 py-2 mb-4 text-white bg-blue-500 rounded hover:bg-blue-600"
         >
           Get Started
         </Link>
       </Button>
+      <h2>Land More Interviews, Effortlessly</h2>
+      <p className="text-xl">
+        Upload your resume, add a job description, and let Resume Bueno do the
+        rest. Get a match score, targeted resume improvements, and a polished
+        cover letter, instantly.
+      </p>
     </div>
   )
 }
