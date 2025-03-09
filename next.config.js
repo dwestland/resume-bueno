4f7ff58b616d3b07dcb313a014ac22819704c64c
dwestland <don@westland.net>
2025-03-08 21:03:40 -0800

    Update files

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Enable server components external packages
-  serverExternalPackages: ['pdf-parse'],
-}
-
-module.exports = nextConfig
